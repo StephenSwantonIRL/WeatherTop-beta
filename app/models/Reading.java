9f2dd8172cb46e7d69d867ab4e95a98a0c66c6db
Stephen <stephenswanton@gmail.com>
2021-05-01 16:29:35 +0100

    Added Reading Model

---
+++ app/models/Reading.java
@@ -0,0 +1,33 @@
+package models;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Reading extends Model {
+    public int code ;
+    public float temperature ;
+    public float windSpeed ;
+    public float windDirection ;
+    public int pressure ;
+    public String timestamp ;
+    
+    public Reading(int code, float temperature, float windSpeed, float windDirection, int pressure)
+    {
+        this.code = code;
+        this.temperature = temperature;
+        this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
+        this.pressure = pressure;
+        LocalDateTime timeNow = LocalDateTime.now();
+        DateTimeFormatter dateFormatPattern = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        this.timestamp = timeNow.format(dateFormatPattern);
+
+
+
+    }
+}
