dada45c2a58501293bce2694313406762e6b76fd
Stephen <stephenswanton@gmail.com>
2021-05-01 14:59:25 +0100

    Implement Log In v1

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -3,10 +3,16 @@ package controllers;
 import play.Logger;
 import play.mvc.Controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import models.Member;
+
 public class Dashboard extends Controller
 {
   public static void index() {
     Logger.info("Rendering Dashboard");
+    Member member = Accounts.getLoggedInMember();
+    /*List<Station> stations = member.stations;*/
     render ("dashboard.html");
   }
 }
